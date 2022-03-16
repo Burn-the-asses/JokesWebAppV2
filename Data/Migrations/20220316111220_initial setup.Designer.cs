6c85682219816d52751aff30b93cbc5d98829317
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-03-16 16:17:00 +0500

    Database created and updated without testing

--- Data/Migrations/20220316111220_initial setup.Designer.cs
+++ Data/Migrations/20220316111220_initial setup.Designer.cs
@@ -4,14 +4,16 @@ using JokesWebAppV2.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace JokesWebAppV2.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220316111220_initial setup")]
+    partial class initialsetup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
